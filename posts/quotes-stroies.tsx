8cf461149f8556e6acc050999865f2a3e0dc0784
Yodarango <danranngel@gmail.com>
2021-08-20 20:00:01 -0400

    added the stories

--- posts/quotes-stroies.tsx
+++ posts/quotes-stroies.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // components
 
@@ -24,11 +24,35 @@ export type quoteStoriesProps = {
 };
 
 const QuoteStories = ({ stories }: quoteStoriesProps) => {
-   // ============== FUNCTION 1: Open the stories of Each user
+   // ==============   FUNCTION 1: Open the stories of Each user   =============== //
+   const [handleStoriePopupState, setHandleStoriePopupState] = useState<boolean>(false);
+   const [countState, setCountState] = useState<number>(0);
+   const handleOpenStroies = (stories: Tstory) => {
+      document.body.style.overflow = "hidden";
+      setHandleStoriePopupState(true);
+   };
 
+   // ==============   FUNCTION 2: Go backwards in the story   =============== //
+   const handleMoveBack = () => {
+      if (countState > 0) setCountState(countState - 1);
+   };
+
+   // ==============   FUNCTION 3: Go forthward in the story   =============== //
+   const handleMoveForth = () => {
+      if (countState < stories.stories.length - 1) setCountState(countState + 1);
+   };
+
+   // ==============   FUNCTION 4: close all the stories   =============== //
+   const handleCloseStories = () => {
+      document.body.style.overflow = "scroll";
+      setHandleStoriePopupState(false);
+      setCountState(0);
+   };
    return (
       <div className={quoteStoriesStyles.mainWrapper}>
-         <section className={quoteStoriesStyles.mainStoryWrapper}>
+         <section
+            className={quoteStoriesStyles.mainStoryWrapper}
+            onClick={() => handleOpenStroies(stories)}>
             <div
                className={quoteStoriesStyles.userReputationWrapper}
                style={{
@@ -40,15 +64,40 @@ const QuoteStories = ({ stories }: quoteStoriesProps) => {
             </div>
             <p className={quoteStoriesStyles.userSignature}>{stories.userId}</p>
          </section>
-         <section className={quoteStoriesStyles.storyPostWrapper}>
-            {stories.stories.map((post) => (
+         {handleStoriePopupState && (
+            <section className={quoteStoriesStyles.storyPostWrapper}>
+               <div
+                  className={quoteStoriesStyles.avatarImageStory}
+                  style={{ backgroundImage: `url(${stories.userAvatar})` }}></div>
+               <div className={quoteStoriesStyles.count}>
+                  {countState + 1} of {stories.stories.length}
+               </div>
+               <div
+                  className={`closeModal ${quoteStoriesStyles.closeModal}`}
+                  onClick={handleCloseStories}>
+                  X
+               </div>
+               <div className={quoteStoriesStyles.storyPostsControllerWrapper}>
+                  <span
+                     className={quoteStoriesStyles.postStoryLeft}
+                     onClick={handleMoveBack}></span>
+                  <span
+                     className={quoteStoriesStyles.postStoryRight}
+                     onClick={handleMoveForth}></span>
+               </div>
                <div
                   className={`${quoteStoriesStyles.storyPost}`}
-                  style={{ backgroundImage: post.background }}>
-                  {post.content}
+                  style={{ backgroundImage: stories.stories[countState].background }}>
+                  <p className={`${quoteStoriesStyles.storyContent}`}>
+                     {stories.stories[countState].content}{" "}
+                     <span className={quoteStoriesStyles.quotationMark}></span>
+                  </p>
+                  <span className={quoteStoriesStyles.storyBy}>
+                     -By: {stories.stories[countState].by}
+                  </span>
                </div>
-            ))}
-         </section>
+            </section>
+         )}
       </div>
    );
 };
