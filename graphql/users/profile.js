53b6cc02048ccfc62f57656b786acff441dda7d0
Yodarango <danranngel@gmail.com>
2021-11-27 21:42:01 -0500

    added content for user profile

--- graphql/users/profile.js
+++ graphql/users/profile.js
@@ -1,7 +1,7 @@
 import { gql } from "@apollo/client";
 
 export const GET_PROFILE_INFO = gql`
-   query ($ID: ID, $totalCountOnly: Boolean) {
+   query ($ID: ID, $totalCountOnly: Boolean, $getApprovalCount: Boolean) {
       users(ID: $ID) {
          ID
          MONGO_DB_ID
@@ -23,7 +23,10 @@ export const GET_PROFILE_INFO = gql`
          my_story
          my_ministry
          my_favorite_verse
-         all_posts {
+         all_posts(USER_ID: $ID, getApprovalCount: $getApprovalCount) {
+            thought_approval_total_count
+            quote_approval_total_count
+            commentaries_approval_total_count
             commentaries(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
                total_count
             }
@@ -35,6 +38,101 @@ export const GET_PROFILE_INFO = gql`
             }
             sermon_notes(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
                total_count
+            }
+         }
+      }
+   }
+`;
+
+export const GET_PROFILE_COMMENTARIES = gql`
+   query ($ID: ID, $totalCountOnly: Boolean) {
+      users(ID: $ID) {
+         all_posts(USER_ID: $ID) {
+            #============== COMMENTARIES ============
+            commentaries(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
+               ID
+               USER_ID
+               VERSE_ID
+               body
+               category_tags
+               referenced_verses
+               verse_citation
+               created_date
+               posted_on
+               comments {
+                  total_count
+               }
+               approvals {
+                  average_count
+                  total_count
+               }
+            }
+         }
+      }
+   }
+`;
+
+export const GET_PROFILE_QUOTES = gql`
+   query ($ID: ID, $totalCountOnly: Boolean) {
+      users(ID: $ID) {
+         all_posts(USER_ID: $ID) {
+            #============== QUOTES ============
+            quotes(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
+               ID
+               USER_ID
+               body
+               category_tags
+               author
+               background
+               created_date
+               posted_on
+               comments {
+                  total_count
+               }
+               approvals {
+                  average_count
+                  total_count
+               }
+            }
+         }
+      }
+   }
+`;
+
+export const GET_PROFILE_THOUGHTS = gql`
+   query ($ID: ID, $totalCountOnly: Boolean) {
+      users(ID: $ID) {
+         all_posts(USER_ID: $ID) {
+            #============== THOUGHTS ============
+            thoughts(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
+               ID
+               body
+               USER_ID
+               category_tags
+               posted_on
+               comments {
+                  total_count
+               }
+               approvals {
+                  average_count
+                  total_count
+               }
+            }
+         }
+      }
+   }
+`;
+
+export const GET_PROFILE_SERMON_NOTES = gql`
+   query ($ID: ID, $totalCountOnly: Boolean) {
+      users(ID: $ID) {
+         all_posts(USER_ID: $ID) {
+            #============== SERMONS ============
+            sermon_notes(totalCountOnly: $totalCountOnly, USER_ID: $ID) {
+               ID
+               content
+               USER_ID
+               category_tags
             }
          }
       }
