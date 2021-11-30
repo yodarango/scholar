0b72d93ab523a8f1ca30fd37cf52727739944e40
Yodarango <danranngel@gmail.com>
2021-11-29 20:54:57 -0500

    added pagination for all content types

---
+++ layouts/home-page-content/sermon-notes-content.tsx
@@ -0,0 +1,73 @@
+// core
+import React, { useState, useEffect } from "react";
+
+//graphQL
+import client from "../../apollo-client";
+import { GET_PROFILE_SERMON_NOTES } from "../../graphql/users/profile";
+
+// comps
+import SermonNotesPost from "../../posts/sermon-notes-post";
+
+//styles
+import homePageContentStyles from "../../styles/layouts/home-page-content/HomePageContent.module.css";
+
+// helpers / state
+import { TsermonPost } from "../../posts/sermon-notes-post";
+import { Tuser } from "../../pages/users/[...userId]";
+
+type sermonNotesContentProps = {
+   user: Tuser;
+   handleCloseSermonNotes: any;
+};
+
+const SermonNotesContent = ({ user, handleCloseSermonNotes }: sermonNotesContentProps) => {
+   const [sermonState, setsermonState] = useState<TsermonPost[]>([]);
+   const [sermonNotesLastIdState, setSermonNotesLastIdState] = useState<string>("99999999999");
+   const [hideLoadMoreBttnState, setHideLoadMoreBttnState] = useState<boolean>(false);
+
+   useEffect(() => {
+      const requestSermons = async () => {
+         const { data } = await client.query({
+            query: GET_PROFILE_SERMON_NOTES,
+            variables: { ID: user.ID, totalCountOnly: false, last_id: sermonNotesLastIdState }
+         });
+
+         setsermonState((sermonState) => [...sermonState, ...data.users[0].all_posts.sermon_notes]);
+         data.users[0].all_posts.sermon_notes.length < 20 ? setHideLoadMoreBttnState(true) : null;
+      };
+      requestSermons();
+   }, [sermonNotesLastIdState]);
+
+   return (
+      <div className={"dark-bkg"}>
+         <span className={"closeModal"} onClick={handleCloseSermonNotes}>
+            X
+         </span>
+         <section className={homePageContentStyles.popUpContentWrapper}>
+            <h1 className={homePageContentStyles.popUpContentWrapper_title}>
+               Sermons by {user.signature}
+            </h1>
+            {sermonState.map((sermon: TsermonPost) => (
+               <section>
+                  <SermonNotesPost
+                     key={sermon.ID}
+                     sermonPost={sermon}
+                     deleteOption={true}
+                     editOption={true}
+                     reportOption={true}
+                  />
+               </section>
+            ))}
+            {!hideLoadMoreBttnState && (
+               <button
+                  className={"std-button"}
+                  onClick={() => setSermonNotesLastIdState(sermonState[sermonState.length - 1].ID)}>
+                  <p className='std-button_gradient-text'>Load More</p>
+               </button>
+            )}
+         </section>
+      </div>
+   );
+};
+
+export default SermonNotesContent;
---
+++ layouts/home-page-content/thoughts-content.tsx
@@ -0,0 +1,75 @@
+// core
+import React, { useState, useEffect } from "react";
+
+//graphQl
+import client from "../../apollo-client";
+import { GET_PROFILE_THOUGHTS } from "../../graphql/users/profile";
+
+// comps
+import Thought from "../../posts/thought";
+
+// styles
+import homePageContentStyles from "../../styles/layouts/home-page-content/HomePageContent.module.css";
+
+// helpers types
+import { Tuser } from "../../pages/users/[...userId]";
+import { Tthought } from "../../posts/thought";
+
+type thoughtContentProps = {
+   user: Tuser;
+   handleCloseThoughts: any;
+};
+const ThoughtsContent = ({ user, handleCloseThoughts }: thoughtContentProps) => {
+   const [thoughtsState, setThoughtsState] = useState<Tthought[]>([]);
+   const [thoughtLastIdState, setThoughtLastIdState] = useState<string>("99999999999");
+   const [hideLoadMoreBttnState, setHideLoadMoreBttnState] = useState<boolean>(false);
+
+   useEffect(() => {
+      const requestThoughts = async () => {
+         const { data } = await client.query({
+            query: GET_PROFILE_THOUGHTS,
+            variables: { ID: user.ID, totalCountOnly: false, last_id: thoughtLastIdState }
+         });
+
+         // add user values to each thought before passing it to the component
+         const modifiedThoughts: any = [];
+         data.users[0].all_posts.thoughts.map((thought: Tthought) =>
+            modifiedThoughts.push({
+               ...thought,
+               creator: { ID: user.ID, avatar: user.avatar, signature: user.signature }
+            })
+         );
+         setThoughtsState((thoughtsState) => [...thoughtsState, ...modifiedThoughts]);
+         data.users[0].all_posts.thoughts.length < 20 ? setHideLoadMoreBttnState(true) : null;
+      };
+      requestThoughts();
+   }, [thoughtLastIdState]);
+
+   return (
+      <div className={"dark-bkg"}>
+         <span className={"closeModal"} onClick={handleCloseThoughts}>
+            X
+         </span>
+         <section className={homePageContentStyles.popUpContentWrapper}>
+            <h1 className={homePageContentStyles.popUpContentWrapper_title}>
+               Thoughts by {user.signature}
+            </h1>
+            <Thought
+               thoughts={thoughtsState}
+               deleteOption={true}
+               editOption={true}
+               reportOption={true}
+            />
+            {!hideLoadMoreBttnState && (
+               <button
+                  className={"std-button"}
+                  onClick={() => setThoughtLastIdState(thoughtsState[thoughtsState.length - 1].ID)}>
+                  <p className='std-button_gradient-text'>Load More</p>
+               </button>
+            )}
+         </section>
+      </div>
+   );
+};
+
+export default ThoughtsContent;
