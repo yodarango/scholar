8369586dff0aa92100f8f11f50645b574e1e3594
Yodarango <danranngel@gmail.com>
2022-05-05 16:48:13 -0400

    small fixes

--- apollo-client.js
+++ apollo-client.js
@@ -29,7 +29,7 @@ const errorLink = onError(({ graphQLErrors, networkError, response, operation })
 });
 
 const client = new ApolloClient({
-   uri: "http://localhost:4000/api",
+   uri: process.env.BACKEND_SOURCE,
    //uri: "http://172.20.10.7:4000/api", //iphone
    //uri: "http://192.168.1.16:4000/api",
    //uri: "https://scholar-be-2.herokuapp.com/api",
