b91fece5fd568d2dd7ee9d5647b352dd7dea1f0f
Yodarango <danranngel@gmail.com>
2022-05-11 17:53:53 +0000

    added ssl to be link

--- apollo-client.js
+++ apollo-client.js
@@ -30,7 +30,7 @@ const errorLink = onError(({ graphQLErrors, networkError, response, operation })
 
 const client = new ApolloClient({
    //uri: process.env.NEXT_PUBLIC_BACKEND_SOURCE,
-   uri: "http://96.30.194.79:4000/api",
+   uri: "https://localhost:4000/api",
    //uri: "http://172.20.10.7:4000/api", //iphone
    //uri: "http://192.168.1.16:4000/api",
    //uri: "https://scholar-be-2.herokuapp.com/api",
