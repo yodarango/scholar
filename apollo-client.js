0ae6dde74d7ff3eecf887eb56b95db259330d2f4
Yodarango <danranngel@gmail.com>
2022-05-12 14:29:21 +0000

    hooked be and fe, both working

--- apollo-client.js
+++ apollo-client.js
@@ -30,7 +30,7 @@ const errorLink = onError(({ graphQLErrors, networkError, response, operation })
 
 const client = new ApolloClient({
    //uri: process.env.NEXT_PUBLIC_BACKEND_SOURCE,
-   uri: "http://localhost:4000/api",
+   uri: "https://my.biblescholar.app/api",
    //uri: "http://172.20.10.7:4000/api", //iphone
    //uri: "http://192.168.1.16:4000/api",
    //uri: "https://scholar-be-2.herokuapp.com/api",
