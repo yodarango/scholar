256b69445aa35fea482059ab79cb0ed3967e0ca0
Yodarango <danranngel@gmail.com>
2022-05-19 21:48:07 -0400

    fixed registratino issue

--- apollo-client.js
+++ apollo-client.js
@@ -51,8 +51,8 @@ const errorLink = onError(({ graphQLErrors, networkError, response, operation })
 });
 
 const client = new ApolloClient({
-   //uri: "https://my.biblescholar.app/api",
-   uri: "http://localhost:4000/api",
+   uri: "https://my.biblescholar.app/api",
+   // uri: "http://localhost:4000/api",
    cache: new InMemoryCache(),
    defaultOptions: defaultOptions,
    //credentials: "include",
