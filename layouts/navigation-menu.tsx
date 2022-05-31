500bd29f9f9b3fcd33d43aaafdacf2e64b97cb6b
Yodarango <danranngel@gmail.com>
2022-05-31 07:45:46 -0400

    remove libe from mobile version

--- layouts/navigation-menu.tsx
+++ layouts/navigation-menu.tsx
@@ -1,5 +1,5 @@
 // core
-import React from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 // components
@@ -8,16 +8,33 @@ import Link from "next/link";
 import navigationMenuStyles from "../styles/layouts/NavigationMenu.module.css";
 
 const NavigationMenu = () => {
+   const [displayLib, setDisplayLib] = useState<boolean>(true);
+   // check if user is on mobile
+   const goFullscreen = () => {
+      var ua = navigator.userAgent;
+      var isMobile = /Android|webOS|iPhone|iPad|iPod/i.test(ua);
+
+      if (isMobile) {
+         setDisplayLib(false);
+      }
+   };
+
+   useEffect(() => {
+      goFullscreen();
+   }, []);
+
    return (
       <div className={`${navigationMenuStyles.mainWrapper}`}>
          <div className={navigationMenuStyles.subWrapper}>
             <Link href={"/"}>
                <a className={`${navigationMenuStyles.home} ${navigationMenuStyles.menuOption}`}></a>
             </Link>
-            <Link href={"/library"}>
-               <a
-                  className={`${navigationMenuStyles.library} ${navigationMenuStyles.menuOption}`}></a>
-            </Link>
+            {displayLib && (
+               <Link href={"/library"}>
+                  <a
+                     className={`${navigationMenuStyles.library} ${navigationMenuStyles.menuOption}`}></a>
+               </Link>
+            )}
             <Link href={"/wigo"}>
                <a className={`${navigationMenuStyles.feed} ${navigationMenuStyles.menuOption}`}></a>
             </Link>
