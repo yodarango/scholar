ce01084212bc9d0ee15e2b7fcabff32489bcc55f
Yodarango <danranngel@gmail.com>
2022-07-27 19:05:09 -0400

    implement lazy loading and error handling on the daily_vese_card and lok at the bible_verse_picker for notes

--- fragments/cards/bible_verse_picker.tsx
+++ fragments/cards/bible_verse_picker.tsx
@@ -1,3 +1,9 @@
+/********************************************************************************************* 
+-  This is the final step component when selecting a new passage of the Bible. When the verse
+   is selected the "handleSelection" function will make the call to the API if stopAtVerse 
+   == true or return the verseId if the stopAtVese == false
+*********************************************************************************************/
+
 import styles from "./bible_verse_picker.module.css";
 
 // comps
@@ -10,6 +16,7 @@ import { chosenKey } from "../../helpers/APIs/select-random-api-key";
 
 type TBibleVersePickerProps = {
    chapterId: string;
+   stopAtVerse: boolean;
    verseCount: number;
    versionId: string;
    cta: {
@@ -23,7 +30,8 @@ export const BibleVersePicker = ({
    verseCount,
    chapterId,
    cta,
-   versionId
+   versionId,
+   stopAtVerse
 }: TBibleVersePickerProps) => {
    // ------------- make the call to the Bible APi
    const handleSelection = async (verseId: string, versionId: string) => {
@@ -57,6 +65,14 @@ export const BibleVersePicker = ({
       }
    };
 
+   // ? bring on the fetch verse helper to get rif of the fetch call and also make sure the the new added prop "stopAtVerse"
+   //? did not break anything. if the stopAtVerse == true the component will make the API call, otherwise it will only return
+   //? the verseid
+
+   const handleSelection = () => {
+      if (stopAtVerse) {
+      }
+   };
    return (
       <div className={styles.mainWrapper}>
          <div className={styles.close}>
